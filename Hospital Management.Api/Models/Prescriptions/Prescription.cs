29e2fd51a5606ec4f23c75cf226cbe84a4b3a8c0
Dipanwita Sen <130593147+DipanwitSen@users.noreply.github.com>
2026-02-06 05:55:59 +0530

    Add properties to Prescription model

--- Hospital Management.Api/Models/Prescriptions/Prescription.cs
+++ Hospital Management.Api/Models/Prescriptions/Prescription.cs
@@ -2,5 +2,11 @@
 {
     public class Prescription
     {
+        public int Id { get; set; }
+
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+
+        public DateTime DateIssued { get; set; }
     }
 }
