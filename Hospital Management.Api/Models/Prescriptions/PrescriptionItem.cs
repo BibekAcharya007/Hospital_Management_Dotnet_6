d1e7a24a36cdd1e9cb82ba7e42d5aba8b1bbd978
Dipanwita Sen <130593147+DipanwitSen@users.noreply.github.com>
2026-02-06 05:56:51 +0530

    Update PrescriptionItem.cs

--- Hospital Management.Api/Models/Prescriptions/PrescriptionItem.cs
+++ Hospital Management.Api/Models/Prescriptions/PrescriptionItem.cs
@@ -2,5 +2,12 @@
 {
     public class PrescriptionItem
     {
+        public int Id { get; set; }
+
+        public int PrescriptionId { get; set; }
+        public int MedicineId { get; set; }
+
+        public string? Dosage { get; set; }
+        public int DurationDays { get; set; }
     }
 }
