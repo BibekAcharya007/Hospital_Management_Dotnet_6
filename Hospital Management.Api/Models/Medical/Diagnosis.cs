7d9b1700cf202245b721e80316545521139321f4
Dipanwita Sen <130593147+DipanwitSen@users.noreply.github.com>
2026-02-06 06:01:54 +0530

    Add properties to Diagnosis model

--- Hospital Management.Api/Models/Medical/Diagnosis.cs
+++ Hospital Management.Api/Models/Medical/Diagnosis.cs
@@ -2,5 +2,10 @@
 {
     public class Diagnosis
     {
+        public int Id { get; set; }
+        public int MedicalRecordId { get; set; }
+
+        public string? Description { get; set; }
+       public MedicalRecord? MedicalRecord { get; set; }
     }
 }
