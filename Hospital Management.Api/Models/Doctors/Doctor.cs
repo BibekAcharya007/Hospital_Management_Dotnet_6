65624155470b589a4ac041057b66c57ecab089f2
Dipanwita Sen <130593147+DipanwitSen@users.noreply.github.com>
2026-02-06 05:58:33 +0530

    Add properties to Doctor model

--- Hospital Management.Api/Models/Doctors/Doctor.cs
+++ Hospital Management.Api/Models/Doctors/Doctor.cs
@@ -2,5 +2,13 @@
 {
     public class Doctor
     {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+
+        public int DepartmentId { get; set; }
+        public int SpecializationId { get; set; }
+
+        public Department? Department { get; set; }
+        public DoctorSpecialization? Specialization { get; set; }
     }
 }
