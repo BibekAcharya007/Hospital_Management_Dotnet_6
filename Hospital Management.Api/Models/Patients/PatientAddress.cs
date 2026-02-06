035bbe146a7a4850ab95356951bbd7218b50b179
Dipanwita Sen <130593147+DipanwitSen@users.noreply.github.com>
2026-02-06 06:06:17 +0530

    Add properties to PatientAddress model

--- Hospital Management.Api/Models/Patients/PatientAddress.cs
+++ Hospital Management.Api/Models/Patients/PatientAddress.cs
@@ -1,6 +1,13 @@
-﻿namespace Hospital_Management.Api.Models.Patients
+namespace Hospital_Management.Api.Models.Patients
 {
     public class PatientAddress
     {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public string? AddressLine { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public Patient Patient { get; set}//object
+
     }
 }
