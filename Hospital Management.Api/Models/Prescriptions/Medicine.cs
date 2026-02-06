aff6fdd97f60ca021a5c764d94a7ed1e66942745
Dipanwita Sen <130593147+DipanwitSen@users.noreply.github.com>
2026-02-06 05:54:29 +0530

    Update Medicine.cs

--- Hospital Management.Api/Models/Prescriptions/Medicine.cs
+++ Hospital Management.Api/Models/Prescriptions/Medicine.cs
@@ -2,5 +2,11 @@
 {
     public class Medicine
     {
+        public int Id { get; set; }
+
+       public int PatientId { get; set; }
+       public int DoctorId { get; set; }
+
+       public DateTime DateIssued { get; set; }
     }
 }
