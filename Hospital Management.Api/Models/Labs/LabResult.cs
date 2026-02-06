f2df6a36acaf583fbb9796c6ac458cec3170bf29
Dipanwita Sen <130593147+DipanwitSen@users.noreply.github.com>
2026-02-06 06:00:36 +0530

    Add properties to LabResult model

--- Hospital Management.Api/Models/Labs/LabResult.cs
+++ Hospital Management.Api/Models/Labs/LabResult.cs
@@ -2,5 +2,12 @@
 {
     public class LabResult
     {
+        public int Id { get; set; }
+
+        public int PatientId { get; set; }
+        public int LabTestId { get; set; }
+
+        public string? Result { get; set; }
+        public DateTime TestDate { get; set; }
     }
 }
