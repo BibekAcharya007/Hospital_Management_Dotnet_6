c8f52aab3b9e53d82b28b0a3afd29a1df9504b23
Dipanwita Sen <130593147+DipanwitSen@users.noreply.github.com>
2026-02-06 05:46:40 +0530

    Add AdmissionDate property to Admission model

--- Hospital Management.Api/Models/Admissions/Admission.cs
+++ Hospital Management.Api/Models/Admissions/Admission.cs
@@ -1,6 +1,12 @@
-﻿namespace Hospital_Management.Api.Models.Admissions
+using System;
+
+namespace Hospital_Management.Api.Models.Admissions
 {
     public class Admission
     {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+
+        public DateTime AdmissionDate { get; set; }
     }
 }
