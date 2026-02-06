72249474f1b20529e1007cd15028a6201be72feb
Dipanwita Sen <130593147+DipanwitSen@users.noreply.github.com>
2026-02-06 06:04:58 +0530

    Add properties to MedicalRecord class

--- Hospital Management.Api/Models/Patients/MedicalRecord.cs
+++ Hospital Management.Api/Models/Patients/MedicalRecord.cs
@@ -2,5 +2,15 @@
 {
     public class MedicalRecord
     {
+        public int Id { get; set; }
+
+       public int PatientId { get; set; }
+       public int DoctorId { get; set; }
+
+       public string? Notes { get; set; }
+       public DateTime CreatedAt { get; set; }
+
+       public Patient Patient { get; set; }
+       public Doctor Doctor { get; set; }
     }
 }
