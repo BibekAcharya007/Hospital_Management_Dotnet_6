8e79bbe3a2fb83c59dd0736c7faf6ab747976572
Dipanwita Sen <130593147+DipanwitSen@users.noreply.github.com>
2026-02-06 05:48:33 +0530

    Update Appointment.cs

--- Hospital Management.Api/Models/Appointments/Appointment.cs
+++ Hospital Management.Api/Models/Appointments/Appointment.cs
@@ -2,5 +2,16 @@
 {
     public class Appointment
     {
+        public int Id { get; set; }
+
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+
+        public int StatusId { get; set; }
+
+        public Patient? Patient { get; set; }
+        public Doctor? Doctor { get; set; }
+        public AppointmentStatus? Status { get; set; }
     }
 }
