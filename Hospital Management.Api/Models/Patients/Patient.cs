090dd7f7f409e90f639026bf4112f05dcaa3fb66
Dipanwita Sen <130593147+DipanwitSen@users.noreply.github.com>
2026-02-06 06:05:34 +0530

    Enhance Patient model with additional properties
    Added properties for patient details including contact information and entry time.

--- Hospital Management.Api/Models/Patients/Patient.cs
+++ Hospital Management.Api/Models/Patients/Patient.cs
@@ -1,7 +1,21 @@
-﻿namespace Hospital_Management.Api.Models.Patients
+using System;
+using System.Collections.Generic;
+namespace Hospital_Management.Api.Models.Patients
 {
     public class Patient
     {
-
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public DateTime DOB { get; set; }
+        public string? Gender { get; set; }
+        public string? BloodGroup { get; set; }
+        public string? Allergies { get; set; }
+        public string? ChronicConditions { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? EmergencyContactName { get; set; }
+        public string? EmergencyConctactPhone { get; set; }
+        public DateTime EntryTime { get; set}
+        public ICollection<PatientAddress> Address { get; set; }
     }
 }
